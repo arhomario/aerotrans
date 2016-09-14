f782f3458959fe8bfc773d554371cb38b49c5c3d
arhomario <ahmadrhomario92@gmail.com>
2016-09-14 08:46:47 +0700

    add bootstrap

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -3,13 +3,12 @@
 <head>
 <title>@yield('title')</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="{{ asset('laravel/resources/assets/bootstrap-3.3.6-dist/css/bootstrap.css') }}" rel="stylesheet">
 
 </head>
 <body>
+  <p class="help-block">a</p>
 
-
-@yield('content')
-
-
+  @yield('content')
 </body>
 </html>
